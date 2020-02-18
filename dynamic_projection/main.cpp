cb99675e9964bc305c20d8c4afbca070a4678fab
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2020-02-18 23:25:43 +0900

    parameter tuning
    todo : read settings file easily

--- dynamic_projection/main.cpp
+++ dynamic_projection/main.cpp
@@ -99,22 +99,23 @@ int main(int argc, char* argv[])
 
 		// move the window to the second display 
 		// (assuming the two displays are top aligned)
-		cvNamedWindow("My Window", CV_WINDOW_AUTOSIZE);
-		moveWindow("My Window", width_first, height_first);
-		cvSetWindowProperty("My Window", WND_PROP_AUTOSIZE, WINDOW_AUTOSIZE);
+		cvNamedWindow("ProjectionWindow", CV_WINDOW_AUTOSIZE);
+		moveWindow("ProjectionWindow", width_first, height_first);
+		cvSetWindowProperty("ProjectionWindow", WND_PROP_AUTOSIZE, WINDOW_AUTOSIZE);
 
 		// create target image
-		Mat img = Mat(Size(width_second, height_second), CV_8UC1);
+		Mat detectionResized = Mat(Size(width_second, height_second), CV_8UC1);
+		Mat projectionResized = Mat(Size(width_second, height_second), CV_8UC1);
 #pragma endregion
 
 		// create circular calibration pattern
-		int radius = 5;
-		int distance = 30;
+		int radius = 25;
+		int distance = 70;
 		int x_offset = 30; int y_offset = 30;
 
 		vector<CvPoint> circles;
-		for (int i = 0; i < 11; i++) {
-			for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
 				CvPoint point{ x_offset + ((i % 2) * distance + (j * distance * 2)), y_offset + (i * distance )};
 				circles.push_back(point);
 			}
@@ -131,20 +132,64 @@ int main(int argc, char* argv[])
 				for (int i = 0; i < size; i++) {
 					vector<Point2f> pointBuf;
 					Mat view = list_of_framesets[i].color_image;
+					cv::cvtColor(view, view, CV_BGR2RGB);
+
+					Mat circlesDisplay(cvSize(view.cols, view.rows), CV_8UC3, Scalar(0));
 			
 					// Draw circles on screen
 					if (MODE == TRACKING_MODE) {
 						for (int i = 0; i < circles.size(); i++) {
-							circle(view, circles[i], radius, CvScalar(255, 255, 255), -1, 8, 0);
+							circle(circlesDisplay, circles[i], radius, CvScalar(255, 255, 255), -1, 8, 0);
 						}
 					}
-					// Detect circles projected by the projector
 					
+					// Detect circles projected by the projector
+#pragma region circle detection
+					Mat gray;
+					cvtColor(view, gray, COLOR_BGR2GRAY);
+					medianBlur(gray, gray, 5);
+
+					vector<Vec3f> circles;
+					HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+						gray.rows / 64,  // change this value to detect circles with different distances to each other
+						100, 30, 1, 30 // change the last two parameters
+				   // (min_radius & max_radius) to detect larger circles
+					);
+#pragma endregion
+#pragma region Display result
+					// Draw found circles on image
+					for (size_t i = 0; i < circles.size(); i++)
+					{
+						Vec3i c = circles[i];
+						Point center = Point(c[0], c[1]);
+						// circle center
+						circle(view, center, 1, Scalar(0, 100, 100), 2, LINE_AA);
+						// circle outline
+						int radius = c[2];
+						circle(view, center, radius, Scalar(255, 0, 255), 2, LINE_AA);
+					}
+
+					// Get 3D coordinates of projected circles in camera coordinate system.
+
+
+					// Get 3D coordinates of projected circles from board coordinates. Back projection
+
+					// computing the instrinsics of the projector because you have 3d points (the projected circles) in world coordinates, and their respective “projection” in projector “image” plane.
+
+					// Matrix decomposition to get K, R, T of projector
+
+					/*
+					Finally, we can start computing the extrinsics of the camera-projector (because basically we have 3d points in “world coordinates” (the board), 
+					which are the projected circles, and also their projection (2d points) in the camera image and projector “image”). 
+					This means you can use the standard stereo calibration routine in openCV
+					*/
 
 					// show image
-					resize(view, img, CvSize(width_second, height_second));
-					imshow("My Window", img);
-
+					resize(view, detectionResized, CvSize(width_second, height_second));
+					resize(circlesDisplay, projectionResized, CvSize(width_second, height_second));
+					imshow("My Window", detectionResized);
+					imshow("ProjectionWindow", projectionResized);
+#pragma endregion
 				}
 			}
 
