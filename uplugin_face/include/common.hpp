a79f86ff9cd2cb2e7614fb4afde20bdae6e7cdbb
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2020-05-24 19:55:12 +0900

    add method to detect faces in Unity

--- uplugin_face/include/common.hpp
+++ uplugin_face/include/common.hpp
@@ -12,4 +12,5 @@ extern "C" {
 	common_api void com_tinker_recognition_setup_camera(recognition* instance);
 	common_api void com_tinker_recognition_get_color_image(recognition* instance, unsigned char * data, int &width, int &height);
 	common_api void com_tinker_recognition_release_camera(recognition* instance);
+	common_api void com_tinker_recognition_detect_faces(recognition* instance, unsigned char * input, unsigned char * processed, int width, int height);
 }
